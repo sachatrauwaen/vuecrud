ed11c95a478f23e8fba632f033564cbe1bc5f686
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2018-07-31 10:05:43 +0200

    list for mobile

--- src/components/list.vue
+++ src/components/list.vue
@@ -16,6 +16,17 @@
             <el-button icon="el-icon-plus" size="small" @click="addRow()" style="margin:10px auto;display:block"></el-button>
         </template>
     </el-table>
+    <template v-else>
+        <el-card  style="margin-bottom:10px;" v-for="row in model" :key="row.id">
+            <oa-fields :model="row" :schema="rowSchema" :connector="connector" :messages="messages"></oa-fields>
+            <div style="padding-top:10px;">
+                <el-button v-for="action in actions" :key="action.name" :icon="action.icon" size="small" @click="action.execute(row)"></el-button>
+            </div>
+        </el-card>
+        <div style="padding-top:10px;">
+            <el-button icon="el-icon-plus" size="small" @click="addRow()" style="margin:10px auto;display:block"></el-button>
+        </div>
+    </template>
 </div>
 </template>
 
@@ -23,117 +34,115 @@
 import VueForms from "../vueforms";
 
 export default {
-  name: "oa-list",
-  props: {
-    value: {},
-    schema: {},
-    messages: Object,
-    prop: String,
-    connector: {}
-  },
-  data: function() {
-    var self = this;
-    return {
-      actions: [
-        {
-          name: self.translate("Delete"),
-          icon: "el-icon-delete",
-          execute: function(row) {
-            var index = self.model.indexOf(row);
-            if (index > -1) {
-              self.model.splice(index, 1);
+    name: "oa-list",
+    props: {
+        value: {},
+        schema: {},
+        messages: Object,
+        prop: String,
+        connector: {}
+    },
+    data: function () {
+        var self = this;
+        return {
+            actions: [{
+                name: self.translate("Delete"),
+                icon: "el-icon-delete",
+                execute: function (row) {
+                    var index = self.model.indexOf(row);
+                    if (index > -1) {
+                        self.model.splice(index, 1);
+                    }
+                }
+            }]
+        };
+    },
+    computed: {
+        model: {
+            get: function () {
+                return this.value;
+            },
+            set: function (val) {
+                this.$emit("input", val);
             }
-          }
+        },
+        rowSchema: function () {
+            return this.schema.items;
+        },
+        columns: function () {
+            var fields = {};
+            var sch = this.schema.items;
+            for (var key in sch.properties) {
+                if (
+                    key != "id" &&
+                    sch.properties[key].type != "array" &&
+                    (!sch.properties[key].hasOwnProperty("x-ui-grid") ||
+                        sch.properties[key]["x-ui-grid"])
+                ) {
+                    fields[key] = sch.properties[key];
+                }
+            }
+            return fields;
+        },
+        isMobile: function () {
+            return VueForms.isMobile();
         }
-      ]
-    };
-  },
-  computed: {
-    model: {
-      get: function() {
-        return this.value;
-      },
-      set: function(val) {
-        this.$emit("input", val);
-      }
     },
-    rowSchema: function() {
-      return this.schema.items;
+    created: function () {
+        var sch = VueForms.jsonSchema.getNotNull(this.schema);
     },
-    columns: function() {
-      var fields = {};
-      var sch = this.schema.items;
-      for (var key in sch.properties) {
-        if (
-          key != "id" &&
-          sch.properties[key].type != "array" &&
-          (!sch.properties[key].hasOwnProperty("x-ui-grid") ||
-            sch.properties[key]["x-ui-grid"])
-        ) {
-          fields[key] = sch.properties[key];
+    methods: {
+        translate: function (text) {
+            if (this.messages && this.messages[text]) return this.messages[text];
+            else return text;
+        },
+        label: function (prop) {
+            var sch = this.schema.items;
+            var name = sch.properties[prop].title ?
+                sch.properties[prop].title :
+                prop.capitalize();
+            if (this.messages && this.messages[name]) return this.messages[name];
+            else return name;
+        },
+        formatter: function (row, column, cellValue) {
+            var schema = VueForms.jsonSchema.getNotNull(
+                this.schema.items.properties[column.property]
+            );
+            if (schema.type == "boolean") {
+                return cellValue ? this.messages["Yes"] : this.messages["No"];
+            } else if (schema.format == "date-time") {
+                if (!cellValue) return "";
+                return moment(cellValue)
+                    .locale("fr")
+                    .format("lll");
+            } else if (schema.enum) {
+                var i = schema.enum.indexOf(cellValue);
+                return this.messages[schema["x-enumNames"][i]] ?
+                    this.messages[schema["x-enumNames"][i]] :
+                    schema["x-enumNames"][i];
+            }
+            return cellValue;
+        },
+        rowClick: function (row, event, column) {
+            if (column.label) {
+                //this.defaultAction.execute(row, event, column);
+                this.$refs.table.toggleRowExpansion(row, true);
+            }
+        },
+        addRow: function () {
+            var row = {};
+            var sch = this.schema.items;
+            for (var key in sch.properties) {
+                row[key] = sch.default;
+            }
+            var model = this.model;
+            if (model === undefined) {
+                model = [];
+            }
+            model.push(row);
+            this.model = model;
+            this.$refs.table.toggleRowExpansion(row, true);
         }
-      }
-      return fields;
-    },
-    isMobile: function() {
-      return VueForms.isMobile();
     }
-  },
-  created: function() {    
-    var sch = VueForms.jsonSchema.getNotNull(this.schema);
-  },
-  methods: {
-    translate: function(text) {
-      if (this.messages && this.messages[text]) return this.messages[text];
-      else return text;
-    },
-    label: function(prop) {
-      var sch = this.schema.items;
-      var name = sch.properties[prop].title
-        ? sch.properties[prop].title
-        : prop.capitalize();
-      if (this.messages && this.messages[name]) return this.messages[name];
-      else return name;
-    },
-    formatter: function(row, column, cellValue) {
-      var schema = VueForms.jsonSchema.getNotNull(
-        this.schema.items.properties[column.property]
-      );
-      if (schema.type == "boolean") {
-        return cellValue ? this.messages["Yes"] : this.messages["No"];
-      } else if (schema.format == "date-time") {
-        if (!cellValue) return "";
-        return moment(cellValue)
-          .locale("fr")
-          .format("lll");
-      } else if (schema.enum) {
-        var i = schema.enum.indexOf(cellValue);
-        return this.messages[schema["x-enumNames"][i]]
-          ? this.messages[schema["x-enumNames"][i]]
-          : schema["x-enumNames"][i];
-      }
-      return cellValue;
-    },
-    rowClick: function(row, event, column) {
-      if (column.label) {
-        //this.defaultAction.execute(row, event, column);
-        this.$refs.table.toggleRowExpansion(row, true);
-      }
-    },
-    addRow: function() {
-      var row = {};
-      var sch = this.schema.items;
-      for (var key in sch.properties) {
-        row[key] = sch.default;
-      }
-      var model = this.model;
-      if (model === undefined) {
-        model = [];
-      }
-      model.push(row);
-      this.model = model;
-      this.$refs.table.toggleRowExpansion(row, true);
-    }
-  }
 };
 </script>
