2b9660364c94ba0da1ef44639bb9b5c349b28db2
Janne Siera <janne.siera@gmail.com>
2018-09-10 15:33:13 +0200

    Clean up vueforms (utils)

--- src/vueforms.js
+++ src/vueforms.js
@@ -1,31 +1,23 @@
 //import jref from 'json-ref-lite'
 
-const VueForms = {}
-VueForms.jsonSchema = {};
-VueForms.jsonSchema.getNotNull = function (schema) {
-  if (schema.oneOf) {
-      var lst = schema.oneOf.filter(function (s) { s.type != "null" });
-      if (lst.length > 0) {
-          return lst[0];
-      } else {
-          return schema;
-      }
-  } else {
-      return schema;
-  }
-};
-VueForms.isMobile = function () {
-  return window.matchMedia("only screen and (max-width: 760px)").matches;
-};
 
- 
- // override .resolve function to prevent stack-overflow issue
-//  var _originalResolvefn = jref.resolve;
-//  VueForms.jsonSchema.resolve = function (json) {
-//      var clone = JSON.parse(JSON.stringify(json)); // create clone because jref.resolve changes the input value; which results (sometimes) in an stack-overflow error if presented a second time 
-//      return _originalResolvefn(clone);
-//  };
+const jsonSchema = {
+    getNotNull: function (schema) {
+        if (schema.oneOf) {
+            var list = schema.oneOf.filter(s => s.type != "null");
+            if(list.length > 0) return list[0];
+        }
+        return schema;
+    }
+}
 
-VueForms.jsonSchema.resolve = function (json) {return json};
+const isMobile = () => window.matchMedia("only screen and (max-width: 760px)").matches;
 
- export default VueForms;
+const resolve = (json) => json;
+
+
+ export default {
+     jsonSchema,
+     isMobile,
+     resolve
+ };
