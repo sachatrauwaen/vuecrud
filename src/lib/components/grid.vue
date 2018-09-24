a330eeabdebf99731ae9aafa6689ebbc48e4f9c0
Janne Siera <janne.siera@gmail.com>
2018-09-24 14:07:18 +0200

    Remove min-width from last column in the grid's table

--- src/lib/components/grid.vue
+++ src/lib/components/grid.vue
@@ -2,7 +2,7 @@
 <div>
     <el-table v-if="!isMobile" :data="model" @row-click="rowClick" style="width: 100%" :row-style="{cursor: 'pointer'}">
         <el-table-column v-for="(value, key) in columns" :key="key" :prop="key" :label="label(key)" :formatter="formatter" class-name="crudcell"></el-table-column>
-        <el-table-column align="right" min-width="120px">
+        <el-table-column align="right">
             <template slot-scope="scope">
                 <el-button v-for="action in actions" :key="action.name" :icon="action.icon" size="small" v-show="actionVisible(action, scope.row, scope.$index)" @click="action.execute(scope.row, scope.$index)"></el-button>
             </template>
