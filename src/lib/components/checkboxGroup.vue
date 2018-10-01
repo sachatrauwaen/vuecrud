f3f0a6e9624355121ff69e586030ec95c03d4add
Janne Siera <janne.siera@gmail.com>
2018-10-01 11:06:58 +0200

    Fix renaming from resource to appService

--- src/lib/components/checkboxGroup.vue
+++ src/lib/components/checkboxGroup.vue
@@ -17,7 +17,7 @@ export default {
     schema: {},
     prop: String,
     connector: {},
-    resource: String,
+    appService: String,
   },
   data() {
     return {
@@ -41,7 +41,7 @@ export default {
       var enumValueField = this.schema["x-enum-valuefield"];
       var enumTextField = this.schema["x-enum-textfield"] || this.schema["x-enum-valuefield"];
       self.connector.service(
-        this.resource,
+        this.appService,
         enumAction,
         {},
         function(data) {
