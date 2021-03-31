4a28650bdd0fe94cc9f2a7f6a760de37b0e12393
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2021-03-31 12:15:51 +0200

    fix row click

--- src/lib/components/grid.vue
+++ src/lib/components/grid.vue
@@ -115,7 +115,7 @@ export default {
             }
             return cellValue;
         },
-        rowClick(row, event, column) {
+        rowClick(row, column, event) {
             if (column.label) {
                 this.defaultAction.execute(row, event, column);
             }
